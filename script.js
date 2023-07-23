406c88b616d909e109d01fad011e23d41b970054
vitaliy-martynyuk <84335906+vitaliy-martynyuk@users.noreply.github.com>
2023-07-23 19:20:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const value1 = document.getElementById("value1");
+const value2 = document.getElementById("value2");
+const result = document.getElementById("result");
+
+const addButton = document.getElementById("add-button");
+const subtractButton = document.getElementById("subtract-button");
+const multiplyButton = document.getElementById("multiply-button");
+const divideButton = document.getElementById("divide-button");
+const greaterButton = document.getElementById("greater-button");
+const greaterOrEqualsButton = document.getElementById(
+  "greater-or-equals-button"
+);
+const lessButton = document.getElementById("less-button");
+const lessOrEqualsButton = document.getElementById("less-or-equals-button");
+const equalsButton = document.getElementById("equals");
+const strictEquals = document.getElementById("strict-equals");
+
+//Підказка: значення інпутів зберігається під value ключем(value1, value2, result - об'єкти)
+//Нагадування: калькулятор для чисел(Number)
+addButton.addEventListener("click", () => {
+  //сюди код для + логіки
+});
+subtractButton.addEventListener("click", () => {
+  //сюди код для - логіки
+});
+multiplyButton.addEventListener("click", () => {
+  //сюди код для * логіки
+});
+divideButton.addEventListener("click", () => {
+  //сюди код для / логіки
+});
+greaterButton.addEventListener("click", () => {
+  //сюди код для > логіки
+});
+greaterOrEqualsButton.addEventListener("click", () => {
+  //сюди код для >= логіки
+});
+lessButton.addEventListener("click", () => {
+  //сюди код для < логіки
+});
+lessOrEqualsButton.addEventListener("click", () => {
+  //сюди код для <= логіки
+});
+equalsButton.addEventListener("click", () => {
+  //сюди код для == логіки
+});
+strictEquals.addEventListener("click", () => {
+  //сюди код для === логіки
+});
